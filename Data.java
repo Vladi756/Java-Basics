8a37ef5b5089beb697165f43ef233e0a7795624d
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-05 13:38:50 +0100

    Add files via upload
    This is a great example program of how to create a new class in Java, and an object with that class. For now, the object contains only fields (data), but soon we will add methods (functions) to it.

---
+++ Data.java
@@ -0,0 +1,17 @@
+public class Data {                             //Name of the class is the name of the file
+    public static void main(String[] args){     // Main function    
+        class DataOnly {                        // Creating a class with fields
+            int i;
+            double d;
+            boolean b;
+        }
+        DataOnly data = new DataOnly();        // Creating a new object with class "DataOnly"
+        data.i = 56;                            // Initailzing those fields to a value
+        data.d = 75.234;
+        data.b = true;
+        
+        System.out.println("Data's int value is: " + data.i);     // Outputting the results.
+        System.out.println("Data's double value is: " + data.d);
+        System.out.println("Data's boolean value is: " + data.b);
+    }
+}
