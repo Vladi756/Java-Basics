751df2221e87a9074706ad7a1bdf8ce9f22f5116
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-06 15:14:08 +0100

    Add files via upload
    A program which essentially flips a coin. Makes use of logical operators.

---
+++ Coin_Flip.java
@@ -0,0 +1,10 @@
+public class Coin_Flip {
+    public static void main(String[] args) {
+        if(Math.random() > 0.5) {               // Math.random() generates random value between 0.0 and 1.0
+            System.out.println("Tails");        // Heads is assigned to anything > 0.5
+        }
+        else {
+            System.out.println("Heads");        // < 0.5 is Tails
+        }
+    }
+}
