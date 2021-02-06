f9621d5a62ace2ececb9688b1171851bb29e3e7f
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-06 16:33:23 +0100

    Update If_Else.java

--- If_Else.java
+++ If_Else.java
@@ -10,12 +10,12 @@ public class If_Else {
         return x < 15 ? x * 100 : x * 10;           // Ternary if- else is an operator, not a statement
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {        // Outputs for every pair of number is insane, both methods work
         System.out.println(ternaryIfElse(9));
-        System.out.println(standardIfElse(9));
-        System.out.println(ternaryIfElse(20));
-        System.out.println(standardIfElse(20));
+        System.out.println(standardIfElse(9));      // Generaly, the ternary operator is much more consice and takes up a lot less space.
+        System.out.println(ternaryIfElse(20));      // Hoewver, the drawback to using it is the comprehension of the code, which is much better in
+        System.out.println(standardIfElse(20));     // the standard if - else statement. (Code is more readable)
         System.out.println(ternaryIfElse(15));
-        System.out.println(standardIfElse(15));     // Outputs for every pair of number is insane, both methods work 
+        System.out.println(standardIfElse(15));      
     }
 }
