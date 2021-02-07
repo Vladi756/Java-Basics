3cdf474e12f90ae1b9972e10a2de31a4a682cb91
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-07 22:40:42 +0100

    Hello_World
    The first thing every programmer does.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        // Main function.
         System.out.print("Hello World!");
     }
 }
