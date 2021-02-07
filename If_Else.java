72280292698a22bf4cf37a4c1d981ae2b79f9fd2
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-07 22:38:32 +0100

    If_Else
    Features the if - else decision structure. The program showcases both the standard, if - else, and the if - else ternary operator.

--- If_Else.java
+++ If_Else.java
@@ -1,16 +1,16 @@
 public class If_Else {                  
-    static int standardIfElse(int x) {              // Standard If Else Method
+    static int standardIfElse(int x) {              // Standard If Else Method.
         if(x < 15) 
             return x * 100;
-        else                                        // Body of if-else statement
+        else                                        // Body of if-else statement.
             return x * 10;
     }                                              
     
-    static int ternaryIfElse(int x) {               // Ternary If Else Metohd
-        return x < 15 ? x * 100 : x * 10;           // Ternary if- else is an operator, not a statement
+    static int ternaryIfElse(int x) {               // Ternary If Else Metohd.
+        return x < 15 ? x * 100 : x * 10;           // Ternary if- else is an operator, not a statement.
     }
     
-    public static void main(String[] args) {        // Outputs for every pair of number is insane, both methods work
+    public static void main(String[] args) {        // Outputs for every pair of number is insane, both methods work.
         System.out.println(ternaryIfElse(9));
         System.out.println(standardIfElse(9));      // Generaly, the ternary operator is much more consice and takes up a lot less space.
         System.out.println(ternaryIfElse(20));      // Hoewver, the drawback to using it is the comprehension of the code, which is much better in
