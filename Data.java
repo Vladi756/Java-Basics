de831928dcd1d76bc7b10e06d2dd04ad2932a91a
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-07 22:34:13 +0100

    Data
    Program features the most basic things such as initializing objects and creating classes in java.

--- Data.java
+++ Data.java
@@ -1,12 +1,12 @@
-public class Data {                             //Name of the class is the name of the file
+public class Data {                             // Name of the class is the name of the file.
     public static void main(String[] args){     // Main function    
-        class DataOnly {                        // Creating a class with fields
+        class DataOnly {                        // Creating a class with fields.
             int i;
             double d;
             boolean b;
         }
         DataOnly data = new DataOnly();        // Creating a new object with class "DataOnly"
-        data.i = 56;                            // Initailzing those fields to a value
+        data.i = 56;                           // Initailzing those fields to a value.
         data.d = 75.235;
         data.b = true;
         
