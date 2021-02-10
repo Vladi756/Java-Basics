e404dae48c08dad8001b94bc7c3d920422831e75
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-10 23:47:08 +0100

    Inheritance
    Any aspiring Java programmer must know and understand this concept.

--- Introduction/Inheritance.java
+++ Introduction/Inheritance.java
@@ -1,8 +1,8 @@
 public class Inheritance {
-    static class Bicycle {                  // Bycicle class
+    static class Bicycle {                                      // Bicycle class.
         int speed = 0;
         int cadence = 0;
-        int gear = 1;                       // The state of the bycicle objects (the fields of the object)
+        int gear = 1;                                           // The state any bicycle objects would have (i.e. their fields).
         
         void speedUp(int speedUp) {
             speed += speedUp;
@@ -18,14 +18,14 @@ public class Inheritance {
         
         void changeGear(int Gear) {
             gear = Gear;
-        }                                       // The behaviour of the bybicla objects (the methods of object)
+        }                                                      // The behaviour any bycicle objects would have (i.e. their methods).
     }
-    static class MountainBike extends Bicycle { // Key word extends means it takes everything (methods and fields) from Bycicle and adds more
+    static class MountainBike extends Bicycle {                // Key word "extends"  - means it takes everything (methods and fields) from Bycicle class and adds more.
         boolean turbo = false;
         
         void turbo(boolean turboSpeed) {
             turbo = turboSpeed;
-        }                                          // Mountain bikes are fast - so they have an additional behaviour called turbo
+        }                                                      // Mountain bikes are fast - so they have an additional behaviour (method) called turbo.
     }
     
     public static void main(String[] args) {
@@ -35,12 +35,12 @@ public class Inheritance {
         B.changeCadence(60);
         B.turbo(true);
         B.speedUp(5);
-        B.changeGear(3);
+        B.changeGear(3);                                       // These methods can be called becuase "Mountain Bike" inherited all the fields and methods from "Bicycles".
         
         A.speedUp(10);
         A.applyBrakes(4);
         A.changeCadence(90);
-        A.changeGear(4);                       // Calling all methods including turbo for B.
+        A.changeGear(4);                                       // Calling all methods including turbo for B.
         
         System.out.println("Bike A's speed: " + A.speed +
                            ". Bike A's cadence: " + A.cadence +
